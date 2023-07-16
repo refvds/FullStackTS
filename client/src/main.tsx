546377001495fbcd7d7527b3de2f68d48c00c87f
refvds <refvdsk@gmail.com>
2023-07-16 15:32:29 +0300

    set: setup client side

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import { Paths } from './paths.ts';
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Home</h1>,
+  },
+  {
+    path: Paths.login,
+    element: <h1>Login</h1>,
+  },
+  {
+    path: Paths.register,
+    element: <h1>register</h1>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
