8f606002a21ef4c181a99640c365d165960d99d8
refvds <refvdsk@gmail.com>
2023-07-16 17:20:23 +0300

    func-layout: add sign up page

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import { Row, Card, Form, Space, Typography } from 'antd';
+import { Link } from 'react-router-dom';
 
-export const Register = () => {
-  return <div>index</div>;
+import { Layout } from '../../components/layout';
+import { CustomInput } from '../../components/custom-input';
+import { PasswordInput } from '../../components/password-input/input';
+import { CustomButton } from '../../components/custom-button';
+import { Paths } from '../../paths';
+
+export const Register: React.FC = () => {
+  return (
+    <Layout>
+      <Row align='middle' justify='center'>
+        <Card style={{ width: '30rem' }} title='Sign up'>
+          <Form onFinish={() => null}>
+            <CustomInput name='name' placeholder='Name' />
+            <CustomInput type='email' name='email' placeholder='Email' />
+            <PasswordInput name='password' placeholder='Password' />
+            <PasswordInput name='confirmPassword' placeholder='Repeat the password' />
+            <CustomButton type='primary' htmlType='submit'>
+              Sing Up
+            </CustomButton>
+          </Form>
+          <Space direction='vertical' size='large'>
+            <Typography.Text>
+              I have already an account <Link to={Paths.login}>Sign In</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
+  );
 };
